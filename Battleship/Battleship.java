767554506ee094f53fd24582417bc38c154ffe5a
Michael J. hoffman <michaeljhoffman@outlook.com>
2024-06-17 00:07:34 -0400

    print grid now working

--- Battleship/Battleship.java
+++ Battleship/Battleship.java
@@ -81,20 +81,21 @@ public class Battleship {
 
 
 		// Use this method to print game boards to the console.
-		private static void printBattleShip(char[][] player) {
-			System.out.print("  ");
-			for (int row = -1; row < 5; row++) {
-				if (row > -1) {
-					System.out.print(row + " ");
-				}
-				for (int column = 0; column < 5; column++) {
-					if (row == -1) {
-						System.out.print(column + " ");
-					} else {
-						System.out.print(player[row][column] + " ");
-				}
-				System.out.println("");
+	private static void printBattleShip(char[][] player) {
+		System.out.print("  ");
+		for (int row = -1; row < 5; row++) {
+			if (row > -1) {
+				System.out.print(row + " ");
 			}
+			for (int column = 0; column < 5; column++) {
+				if (row == -1) {
+					System.out.print(column + " ");
+				} else {
+					System.out.print(player[row][column] + " ");
+				}
+			}
+			System.out.println("");
 		}
 	}
+
 }
