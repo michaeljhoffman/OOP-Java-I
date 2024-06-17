0a1a5d0b82d7ff423b405d9a3b73d0ee497b8f43
Michael J. hoffman <michaeljhoffman@outlook.com>
2024-06-17 00:31:52 -0400

    100 spaces

--- Battleship/Battleship.java
+++ Battleship/Battleship.java
@@ -28,33 +28,39 @@ public class Battleship {
 		// enter coordinates into location boards
 		for (int p=0; p<2; p++){
 			System.out.println("PLAYER " + (p+1) + ", ENTER YOUR SHIPS' COORDINATES.");
-			for (int i=0; i<5; i++) {
-				System.out.println("Enter ship " + (i + 1) + " location:");
-				int[][] tempArray = new int[1][2];
+				for (int i=0; i<5; i++) {
+					System.out.println("Enter ship " + (i + 1) + " location:");
+					int[][] tempArray = new int[1][2];
 
-				for (int j=0; j<2; j++) {
-					while(!in.hasNextInt()) {
-						System.out.println("Invalid coordinates. Choose different coordinates.");
-						in.next();
+					for (int j=0; j<2; j++) {
+						while(!in.hasNextInt()) {
+							System.out.println("Invalid coordinates. Choose different coordinates.");
+							in.next();
+						}
+						
+						tempArray[0][j] = in.nextInt();
+
 					}
-					
-					tempArray[0][j] = in.nextInt();
 
+					int r = tempArray[0][0];
+					int c = tempArray[0][1];
+
+					if (p == 0) {
+						player1LB[r][c] = '@';
+					} else if (p == 1) {
+						player2LB[r][c] = '@';
+					}
 				}
-
-				int r = tempArray[0][0];
-				int c = tempArray[0][1];
-
-				if (p == 0) {
-					player1LB[r][c] = '@';
-				} else if (p == 1) {
-					player2LB[r][c] = '@';
-				}
+			if (p == 0) {
+				printBattleShip(player1LB);
+			} else if (p == 1) {
+				printBattleShip(player2LB);
+			}
+			
+			for (int i=0;i<100;i++){
+				System.out.println();
 			}
 		}
-
-		printBattleShip(player1LB);
-		printBattleShip(player2LB);
 	}
 
 
